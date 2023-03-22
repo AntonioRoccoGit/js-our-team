38fac38669b449ce4ae0c65150b7558bb5bc07bc
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-22 13:02:23 +0100

    add milestone 0

---
+++ js/script.js
@@ -0,0 +1,52 @@
+/*MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede. */
+
+
+ 		        
+ 		        
+const ourTeam = [
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        rule: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela",
+        surname: "Caroll",
+        rule: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter",
+        surname: "Gordon",
+        rule: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela",
+        surname: "Lopez",
+        rule: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott",
+        surname: "Estrada",
+        rule: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara",
+        surname: "Ramos",
+        rule: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    }
+];
