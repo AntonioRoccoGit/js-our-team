c282c8d23e6bdba8f05887ee1add0ef5d6d40f31
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-22 13:10:09 +0100

    add milestone 1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-/*MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console le informazioni di nome, ruolo e la stringa della foto
+/*
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede. */
-
-
- 		        
  		        
 const ourTeam = [
     {
@@ -50,3 +44,13 @@ const ourTeam = [
         img: "barbara-ramos-graphic-designer.jpg"
     }
 ];
+
+console.log(ourTeam);
+for(let i = 0; i < ourTeam.length; i++) {
+    let teamMate = ourTeam[i];
+    console.log(teamMate);
+
+    for(let key in teamMate) {
+        console.log(key, ":", teamMate[key]);
+    }
+}
